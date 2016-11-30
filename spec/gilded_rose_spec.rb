ae975dd21e59ba1b004069bfa51772f0b13b9a85
andy jenkins <andygjenkins@gmail.com>
2016-11-30 14:28:11 +0000

    Add #update_quality tests - edge cases to be tested next

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,25 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "quality of an item is never negative" do
+      items = [Item.new("foo",1,0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+
+    it "sell_in reduced by one" do
+      items = [Item.new("foo", 1, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 0
+    end
+
+    it "sell_in can be negative" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq -1
+    end
+
   end
 
 end
